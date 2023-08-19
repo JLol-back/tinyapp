65908f95d81c606073ce473a0585ee7ddc9008d0
Jake Lollback <jalollback@gmail.com>
2023-08-19 12:07:12 -0500

    Implement redirect after form submission

--- express_server.js
+++ express_server.js
@@ -32,10 +32,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
+  //console.log(req.body); // Log the POST request body to the console
   let shortUrl = generateRandomString();
   urlDatabase[shortUrl] = req.body.longURL;
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortUrl}`); 
 });
 
 app.get("/urls.json", (req, res) => {
