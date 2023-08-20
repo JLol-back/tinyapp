5a57266a25f4822f390e2d7ad02bbb6a967acec7
Jake Lollback <jalollback@gmail.com>
2023-08-20 12:10:58 -0500

    Add delete POST operation and routing

--- express_server.js
+++ express_server.js
@@ -60,16 +60,12 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-/*
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
-*/
+app.post("/urls/:id/delete", (req, res) => { 
+  const id = req.body.id;
+  delete urlDatabase[id];
+  res.redirect("/urls"); 
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
