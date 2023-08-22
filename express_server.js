4ac32311662628cfee281370895332f253427269
Jake Lollback <jalollback@gmail.com>
2023-08-22 18:26:16 -0500

    Create global users obj + add registration handler

--- express_server.js
+++ express_server.js
@@ -25,6 +25,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -44,6 +57,20 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortUrl}`); 
 });
 
+app.post("/register", (req, res) => {
+  let user_id = generateRandomString();
+  console.log(req.body); // Log the POST request body to the console
+  newUser = {
+    id: user_id, 
+    email: req.body["email"], 
+    password: req.body["password"]
+  };
+  users[newUser.id] = newUser;
+  console.log(users);
+  res.cookie("user_id", user_id);
+  res.redirect("/urls"); 
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -51,6 +78,11 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls/new", (req, res) => {
   const templateVars = {username: req.cookies["username"]};
   res.render("urls_new", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = {username: req.cookies["username"]};
+  res.render("register", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
